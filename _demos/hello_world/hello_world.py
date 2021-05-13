e8812e9359b12b1a5ef926279f946f9bf4a6ba01
Hubert <hubert.tournier@gmail.com>
2021-05-14 00:39:48 +0200

    Add files via upload

---
+++ _demos/hello_world/hello_world.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+""" hello_world - say hello to the world
+License: 3-clause BSD (see https://opensource.org/licenses/BSD-3-Clause)
+Author: Hubert Tournier
+"""
+
+import sys
+
+# Version string used by the what(1) and ident(1) commands:
+ID = "@(#) $Id: hello_world - say hello to the world v1.0.0 (May 13, 2021) by Hubert Tournier $"
+
+print("Hello, world!")
+
+sys.exit(0)

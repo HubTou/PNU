9907cb50773db613f2048feaced800ef06d6d372
Hubert Tournier <hubert.tournier@gmail.com>
2024-03-02 00:20:12 +0100

    2024.3.2 update

---
+++ _template/src/COMMAND/library.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+""" COMMAND - ONE_LINE_DESCRIPTION
+License: 3-clause BSD (see https://opensource.org/licenses/BSD-3-Clause)
+Author: YOU
+"""
+
+####################################################################################################
